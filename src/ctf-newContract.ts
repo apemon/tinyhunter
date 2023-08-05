c07a5f08a8247e29bdb120f61ee9b39af21ff873
apemon <760108+apemon@users.noreply.github.com>
2023-08-06 00:46:54 +0700

    add activateBySalt solution

--- src/ctf-newContract.ts
+++ src/ctf-newContract.ts
@@ -1,8 +1,8 @@
-import { Contract } from 'ethers';
+import { Contract, getCreate2Address, keccak256 } from 'ethers';
 
 import { BundleParams, IPendingTransaction } from '@flashbots/mev-share-client';
 import { initMevShare } from './lib/client';
-import { MEV_SHARE_CTF_NEW_CONTRACT_ABI } from './lib/abi';
+import { MEV_SHARE_CTF_NEW_CONTRACT_ABI, MEV_SHARE_CTF_NEW_CONTRACT_CHILD_BYTECODE } from './lib/abi';
 
 const TX_GAS_LIMIT = 500000;
 const MAX_GAS_PRICE = 100n;
@@ -47,8 +47,8 @@ async function backrunHandler(
   };
   const sendBundleResult = await mevShare.sendBundle(bundleParams);
   console.log('bundle hash', sendBundleResult.bundleHash);
-  // const simResult = await mevShare.simulateBundle(bundleParams);
-  // console.log('sim result', simResult);
+  const simResult = await mevShare.simulateBundle(bundleParams);
+  console.log('sim result', simResult);
 }
 
 const ruleFunc = (pendingTx: IPendingTransaction) =>
@@ -56,7 +56,8 @@ const ruleFunc = (pendingTx: IPendingTransaction) =>
     (log) =>
       log.data &&
       log.address === '0x5ea0fea0164e5aa58f407debb344876b5ee10dea' &&
-      log.topics[0] === '0xf7e9fe69e1d05372bc855b295bc4c34a1a0a5882164dd2b26df30a26c1c8ba15'
+      (log.topics[0] === '0xf7e9fe69e1d05372bc855b295bc4c34a1a0a5882164dd2b26df30a26c1c8ba15' ||
+        log.topics[0] === '0x71fd33d3d871c60dc3d6ecf7c8e5bb086aeb6491528cce181c289a411582ff1c')
   );
 
 const main = async () => {
@@ -66,19 +67,30 @@ const main = async () => {
     if (ruleFunc(pendingTx)) {
       try {
         console.log(pendingTx);
-        const [newlyDeployedContract] = contract.interface.decodeEventLog(
-          'Activate',
-          (pendingTx.logs && pendingTx.logs[0].data) || ''
-        )
-        console.log(newlyDeployedContract)
-        backrunHandler(pendingTx.hash, newlyDeployedContract)
-        // [lowerBound, upperBound] = contract.interface.decodeEventLog(
-        //   'Activate',
-        //   (pendingTx.logs && pendingTx.logs[0].data) || '',
-        // );
-        // const currentBlock = await provider.getBlockNumber();
-        // console.log(`lowerBound: ${lowerBound}, upperBound: ${upperBound}`);
-        // backrunHandler(pendingTx.hash, lowerBound, upperBound);
+        if (pendingTx.logs && pendingTx.logs.length > 0) {
+          if (pendingTx.logs[0].topics[0] === '0xf7e9fe69e1d05372bc855b295bc4c34a1a0a5882164dd2b26df30a26c1c8ba15') {
+            const [newlyDeployedContract] = contract.interface.decodeEventLog(
+              'Activate',
+              (pendingTx.logs && pendingTx.logs[0].data) || ''
+            )
+            console.log('newlyDeployedAddress', newlyDeployedContract)
+            // await backrunHandler(pendingTx.hash, newlyDeployedContract)
+          } else if (pendingTx.logs[0].topics[0] === '0x71fd33d3d871c60dc3d6ecf7c8e5bb086aeb6491528cce181c289a411582ff1c') {
+            const [salt] = contract.interface.decodeEventLog(
+              'ActivateBySalt',
+              (pendingTx.logs && pendingTx.logs[0].data) || ''
+            )
+            console.log('salt', salt)
+            // find new address
+            const childContractAddress = getCreate2Address(
+              CONTRACT_ADDRESS,
+              salt,
+              keccak256(MEV_SHARE_CTF_NEW_CONTRACT_CHILD_BYTECODE)
+            )
+            console.log('childContractAddressFromSalt', childContractAddress)
+            await backrunHandler(pendingTx.hash, childContractAddress)
+          }
+        }
       } catch (e) {
         return;
       }
